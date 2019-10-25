ef4797d629ce227f2ac8d35eb99bf94867d4e727
wwy <wuwenyi0807@foxmail.com>
2019-10-25 16:56:37 +0800

    euruka学习â

---
+++ springcloud-eureka/euruka-server/src/main/java/com/wwy/server/EurukaServerApplication.java
@@ -0,0 +1,11 @@
+package com.wwy.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurukaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurukaServerApplication.class, args);
+    }
+}
